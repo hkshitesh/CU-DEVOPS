3eb4ec5dfcb455da35421e323ddda063a5fa891c
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-02-27 12:17:34 +0530

    div function added

--- src/main/java/cu/devops/MyCalc.java
+++ src/main/java/cu/devops/MyCalc.java
@@ -10,12 +10,17 @@ public class MyCalc {
 	{
 		return a-b;
 	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
 	public static void main(String[] args) {
 		
 		
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is "+calc.sum(10, 5));
 		System.out.println("Diff is "+calc.diff(10, 5));
+		System.out.println("Div is "+calc.div(10, 5));
 
 
 	}
